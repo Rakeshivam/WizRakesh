df24b2822d1a6688d3efcdd74aa9d9502c3a526a
Rkesh Kushwaha <rakeshkk589@gmail.com>
2025-08-10 17:49:40 +0530

    Favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,12 +12,12 @@ const inter = Inter({
 export const metadata = {
   title: {
     template:
-      "Next.js Portfolio Created with Three.js and Tailwind CSS | %s | WizRakesh",
+      "WizRakesh",
     default:
-      "Next.js Portfolio Created with Three.js and Tailwind CSS by WizRakesh",
+      "WizRakesh",
   },
   description:
-    "A unique creative portfolio designed by WizRakesh with cutting-edge technologies like Next.js, Tailwind CSS, Three.js, and Framer Motion. Experience the art of modern web development firsthand. Checkout ScriptWizRakesh on youtube.",
+    "A unique creative portfolio designed by WizRakesh with cutting-edge technologies like Next.js, Tailwind CSS, Three.js, and Framer Motion. Experience the art of modern web development firsthand.",
 };
 
 export default function RootLayout({ children }) {
